02efdd0b5eed827352810c40f06b6f3ce565c02a
Ken Umeda <kenumeda1@gmail.com>
2023-01-11 21:46:42 +1100

    make a search box

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import CardList from './Cardlist';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
 
-class App extends React.Component() {
-  render() {
-
-  
-    
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  }
+const App = () => {
+    return (
+        <div>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+        </div>
+    );
 }
 
 export default App;
