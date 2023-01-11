82332d84da051e603e24102935954fa49aa3e549
Ken Umeda <kenumeda1@gmail.com>
2023-01-11 23:08:14 +1100

    Added State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import CardList from './Cardlist';
 import SearchBox from './Searchbox';
 import { robots } from './robots';
 
-
-const App = () => {
-    return (
-        <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
-        </div>
-    );
+const state = {
+    robots:robots,
+    searchfield:''
 }
+
+class App extends React.Component {
+    render(){
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={robots} />
+            </div>
+        );
+        }
+    }
 
 export default App;
